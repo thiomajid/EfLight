c829bee23d6173c68b7d8ee84d32993255a33f2b
thiomajid <82466066+ATOM36@users.noreply.github.com>
2024-07-28 03:01:47 +0100

    Added docs for EfLightOptions

--- EfLight/Core/EfLightOptions.cs
+++ EfLight/Core/EfLightOptions.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 
 namespace EfLight.Core;
+
+/// <summary>
+/// Configuration object to provide when adding services implementing one of EfLight's interfaces.
+/// </summary>
 public class EfLightOptions
 {
     public ServiceLifetime DefaultLifetime { get; set; }
